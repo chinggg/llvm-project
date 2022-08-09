483467cf873e652fa01ff5e1cf937b0b5dfe5b10
chinggg <24590067+chinggg@users.noreply.github.com>
2022-08-09 15:37:38 +0800

    chore: use TransformerClangTidyCheck

--- clang-tools-extra/clang-tidy/bugprone/BomberbirdCheck.h
+++ clang-tools-extra/clang-tidy/bugprone/BomberbirdCheck.h
@@ -9,22 +9,38 @@
 #ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_BUGPRONE_BOMBERBIRDCHECK_H
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_BUGPRONE_BOMBERBIRDCHECK_H
 
-#include "../ClangTidyCheck.h"
+// #include "../ClangTidyCheck.h"
+#include "../utils/TransformerClangTidyCheck.h"
+//#include "clang/Tooling/Transformer/Transformer.h"
+#include "clang/Tooling/Transformer/RangeSelector.h"
+#include "clang/Tooling/Transformer/RewriteRule.h"
+#include "clang/Tooling/Transformer/Stencil.h"
+
 
 namespace clang {
 namespace tidy {
 namespace bugprone {
 
+using namespace clang::tidy::utils;
+using namespace clang::transformer;
+using namespace clang::ast_matchers;
+
+auto BombRule = makeRule(declRefExpr(to(functionDecl(hasName("memcpy")))),
+	changeTo(cat("memcpy_wrapper")),
+    cat("memcpy has been renamed as memcpy_wrapper"));
+
 /// FIXME: Write a short description.
 ///
 /// For the user-facing documentation see:
 /// http://clang.llvm.org/extra/clang-tidy/checks/bugprone-bomberbird.html
-class BomberbirdCheck : public ClangTidyCheck {
+class BomberbirdCheck : public TransformerClangTidyCheck {
+// class BomberbirdCheck : public ClangTidyCheck {
 public:
   BomberbirdCheck(StringRef Name, ClangTidyContext *Context)
-      : ClangTidyCheck(Name, Context) {}
-  void registerMatchers(ast_matchers::MatchFinder *Finder) override;
-  void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+      : TransformerClangTidyCheck(BombRule, Name, Context) {}
+      // : ClangTidyCheck(Name, Context) {}
+  // void registerMatchers(ast_matchers::MatchFinder *Finder) override;
+  // void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
 };
 
 } // namespace bugprone
