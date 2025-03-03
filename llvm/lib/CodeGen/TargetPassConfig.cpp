bbd4b19af657ae1a80cc88df085e6441bdc310e6
chinggg <24590067+chinggg@users.noreply.github.com>
2025-03-03 18:25:41 +0100

    feat: MachineFunctionPass MFCountInstr for all targets
    just use isConditionalBranch() to get cjump insts
    
    (cherry picked from commit eb62832b5c4931970ca300ba093e4a3fdb0af156)

--- llvm/lib/CodeGen/TargetPassConfig.cpp
+++ llvm/lib/CodeGen/TargetPassConfig.cpp
@@ -1288,6 +1288,8 @@ void TargetPassConfig::addMachinePasses() {
   // Add passes that directly emit MI after all other MI passes.
   addPreEmitPass2();
 
+  PM->add(createMFCountInstructionsPass());
+
   AddingMachinePasses = false;
 }
 
